b683529319384556ed891e5a1684a8ae204e6746
Antonio Carlos <antoniocarlos.dev@gmail.com>
2018-03-02 11:38:40 +0100

    Integrating Crashlytics

--- TheCoordinator/AppDelegate.swift
+++ TheCoordinator/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication
         , didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+		
+		Fabric.with([Crashlytics.self])
         
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.appCoordinator = AppCoordinator(window: self.window!, services: Services())
